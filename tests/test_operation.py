7e776c243bd1f2242b33ccbb2e36d284935490c1
Bharat Singh <2k22.cse.32334@gmail.com>
2026-01-31 16:12:01 +0530

    update unit test

---
+++ tests/test_operation.py
@@ -0,0 +1,10 @@
+from src.maths_operation import add,sub
+
+def test_add():
+    assert add(5,2)==7
+    assert add(2,-3)==-1
+
+def test_sub():
+    assert add(5,2)==3
+    assert add(2,-3)==5
+    
